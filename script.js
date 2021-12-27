94e4e96cb4bfd26350b26007d6f7b734f5a84f14
awebdevguy <awebdevguy@gmail.com>
2021-12-27 03:52:41 -0700

    jpg to png

--- script.js
+++ script.js
@@ -123,15 +123,15 @@ function endHandler(e) {
 
 function drawCircle(x, y) {
   context.beginPath();
-  context.arc(x, y, size, 0 , Math.PI * 2);
+  // context.arc(x, y, size, 0 , Math.PI * 2);
 
   // without little circles
-  // context.lineWidth = size;
-  // context.lineCap = 'round';
+  context.lineWidth = size;
+  context.lineCap = 'round';
 
   context.fillStyle = color;
   context.fill();
-  context.closePath();
+  // context.closePath();
 }
 
 function drawLine(x1, y1, x2, y2) {
@@ -141,7 +141,7 @@ function drawLine(x1, y1, x2, y2) {
   context.strokeStyle = color;
   context.lineWidth = size;
   context.stroke();
-  context.closePath();
+  // context.closePath();
 }
 
 // fix canvas blur
@@ -160,6 +160,6 @@ function fix_dpi() {
 // save canvas image to downloads folder
 function download() {
   let download = document.getElementById('download');
-  let image = document.getElementById('canvas').toDataURL('image/jpg').replace('image/jpg', 'image/octet-stream');
+  let image = document.getElementById('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream');
   download.setAttribute('href', image);
 }
